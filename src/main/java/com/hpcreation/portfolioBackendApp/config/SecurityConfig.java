24a4ce56c8ce55cd80b62ab36e9604b56865f920
Harshkumar.Patel <Harshkumar.Patel@valtech.com>
2026-04-02 09:55:18 +0530

    Format code

--- src/main/java/com/hpcreation/portfolioBackendApp/config/SecurityConfig.java
+++ src/main/java/com/hpcreation/portfolioBackendApp/config/SecurityConfig.java
@@ -17,9 +17,16 @@ public class SecurityConfig {
     private final JwtAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authRequest -> authRequest.requestMatchers(HttpMethod.GET, "/**").permitAll().requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll().requestMatchers("/auth/**").permitAll().anyRequest().authenticated()).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
-        }).authenticationEntryPoint(authenticationEntryPoint));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authRequest ->
+                authRequest.requestMatchers(HttpMethod.GET, "/**").permitAll()
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest()
+                    .authenticated()
+            ).oauth2ResourceServer(oauth2 -> oauth2.jwt(_ -> {
+            }).authenticationEntryPoint(authenticationEntryPoint));
         return http.build();
     }
 
